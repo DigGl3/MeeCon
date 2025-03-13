f0d7d2b5b23299ef8abc4b83a97da13abe0cb1c5
DigGl3 <cristianciochina.r@gmail.com>
2025-03-13 09:15:30 +0200

    Business Layer

---
+++ MeeCon.BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MeeCon.Domain.Entities;
+
+namespace MeeCon.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        User Register(string username, string email, string password);
+        User Login(string email, string password);
+    }
+}
---
+++ MeeCon.Web/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using MeeCon.BusinessLogic.Interfaces;
+using MeeCon.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+using System;
+
+namespace MeeCon.Web.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(string username, string email, string password)
+        {
+            try
+            {
+                var user = _authService.Register(username, email, password);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string email, string password)
+        {
+            try
+            {
+                var user = _authService.Login(email, password);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
