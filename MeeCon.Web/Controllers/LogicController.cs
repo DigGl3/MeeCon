aade246fa177b5c8e8ccc9eaf34ad9bb65584bae
DigGl3 <cristianciochina.r@gmail.com>
2025-03-13 15:44:18 +0200

    BL

---
+++ MeeCon.Web/Controllers/LogicController.cs
@@ -0,0 +1,38 @@
+﻿using MeeCon.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeeCon.Web.Controllers
+{
+    public class LogicController : Controller
+    {
+        private readonly ISession _session;
+        public LogicController()
+        {
+            var b1 = new BusinessLogic();
+            _session = b1.GetSessionBL();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public Action Result Index(string username, string password)
+        {
+            try
+            {
+                var user = _session.Login(username, password);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
