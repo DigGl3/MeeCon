a543b5063c48096de978eaa6e7f7cd51c14ff2e5
DigGl3 <cristianciochina.r@gmail.com>
2025-04-10 13:54:10 +0300

    Add REgister and login

--- MeeCon.Web/Controllers/AuthController.cs
+++ MeeCon.Web/Controllers/AuthController.cs
@@ -13,35 +13,100 @@ namespace MeeCon.Web.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly UserApi _userApi;
+
+        public AuthController()
+        {
+            _userApi = new UserApi();
+        }
+
         public ActionResult Login()
         {
-            return View();
-        }
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Register(string fullName, string password)
-        {
-            if(ModelState.IsValid)
+            if (Session["UserId"] != null)
             {
-                var userApi = new UserApi();
-                userApi.RegisterUser(new ULoginData
-                {
-                    Credential = fullName,
-                    Password = password,
-                    LoginIp = Request.UserHostAddress,
-                    LoginDateTime = DateTime.UtcNow
-                });
-
-                return RedirectToAction("Login");
+                return RedirectToAction("Index", "Home");
             }
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(UserDataLogin model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _userApi.LoginUser(new ULoginData
+                {
+                    Credential = model.Username,
+                    Password = model.Password,
+                    LoginIp = Request.UserHostAddress,
+                    LoginDateTime = DateTime.UtcNow
+                });
 
+                if (result.Success)
+                {
+                    Session["UserId"] = result.UserId;
+                    Session["Username"] = result.FullName;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Error = result.StatusMsg;
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            if (Session["UserId"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserDataLogin model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = _userApi.RegisterUser(new ULoginData
+                    {
+                        Credential = model.Username,
+                        Password = model.Password,
+                        LoginIp = Request.UserHostAddress,
+                        LoginDateTime = DateTime.UtcNow,
+                        FullName = model.Username
+                    });
+
+                    if (result.Success)
+                    {
+                        Session["UserId"] = result.UserId;
+                        Session["Username"] = result.FullName;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.Error = result.StatusMsg;
+                        ModelState.AddModelError("", result.StatusMsg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error = "An error occurred during registration: " + ex.Message;
+                    ModelState.AddModelError("", "An error occurred during registration. Please try again.");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
     }
-    
 }
