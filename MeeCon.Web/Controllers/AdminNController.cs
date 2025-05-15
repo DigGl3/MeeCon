db8a73d4b4aef0f5dcdb637d3d80635c2dc0ea2a
DigGl3 <cristianciochina.r@gmail.com>
2025-05-15 10:10:02 +0300

    Bug fix

--- MeeCon.Web/Controllers/AdminNController.cs
+++ MeeCon.Web/Controllers/AdminNController.cs
@@ -17,6 +17,11 @@ namespace MeeCon.Web.Controllers
 
         public ActionResult Index()
         {
+            if (Session["UserId"] == null || (int)Session["UserId"] != 1011)
+            {
+                ViewBag.ErrorMessage = "Trebuie să fii conectat cu userId 1011 pentru a avea acces la această pagină.";
+                return RedirectToAction("Login", "Auth");
+            }
 
             var users = _context.Users.ToList();
             var posts = _context.Posts.ToList();
